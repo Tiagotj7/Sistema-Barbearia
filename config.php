99a5412fcf3c87b55817fd9aa1619fceacffd28f
Tiagotj7 <tiagowork07@gmail.com>
2025-10-16 16:56:39 -0300

    php site ar config edit

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host='localhost:3307';$user='root';$pass='';$db='barbearia';
+$host='localhost:3307';$user='root';$pass='';$db='if0_40183884_barbearia';
 $con=mysqli_connect($host,$user,$pass,$db);
 if(!$con)die('Erro:'.mysqli_connect_error());
 mysqli_set_charset($con,'utf8mb4');
